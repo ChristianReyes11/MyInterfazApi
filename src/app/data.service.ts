877cb698ac3af02f25e96e801ea6d1974bedb4ec
unknown <christianamaro2000@gmail.com>
2021-12-13 14:05:28 -0600

    the finished project is added

---
+++ src/app/data.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { GoalsApi } from './goalsApi';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private goals = new BehaviorSubject<any>(['Jugadores']);
+  goal=  this.goals.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  changeGoal(goal){
+    this.goals.next(goal)
+  }
+
+  apiURL = 'http://34.125.7.41:8102/eduardo-api';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Methods':'GET' 
+      })
+  }  
+
+  
+
+
+  getGoals(): Observable<GoalsApi> {
+    return this.http.get<GoalsApi>(this.apiURL + '/jugadores', this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }   
+
+  newGoal(payload): Observable<GoalsApi> {
+    return this.http.post<GoalsApi>(this.apiURL + '/jugadores', JSON.stringify(payload), this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
+
+
+  handleError(error) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+ }
+
+}
